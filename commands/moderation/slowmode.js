24c4eded51a300ca222bb979ba926b94b0dafcf3
Tehcnalties <45984741+Tehcnalties@users.noreply.github.com>
2021-02-26 21:20:24 -0600

    added number check to slowmode

--- commands/moderation/slowmode.js
+++ commands/moderation/slowmode.js
@@ -11,6 +11,7 @@ module.exports = {
         const seconds = args[0]
 
         if(seconds < 0 || seconds > 26000) return message.channel.send('The slowmode must be between 0 and 26000 seconds!')
+        if(Number.isInteger(seconds) === 'false') return message.channel.send('The slowmode must be an integer number between 0 and 26000!')
 
         message.channel.setRateLimitPerUser(seconds).then(message.channel.send(`Channel slowmode set to ${seconds} seconds.`))
     }
