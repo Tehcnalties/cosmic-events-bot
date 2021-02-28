e0b38bc05e56016702169757aa7439263e4ae8de
Tehcnalties <45984741+Tehcnalties@users.noreply.github.com>
2021-02-28 15:01:08 -0600

    Update embed.js

--- commands/moderation/embed.js
+++ commands/moderation/embed.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 
 module.exports = {
     name: 'embed',
+    category: 'general',
     args: false,
     guildOnly: true,
     async execute(client, message, args) {
