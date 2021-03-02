809a2fc03acf9bd0115516087e447eb1f8616df5
AI2CTURUS <78950532+AI2CTURUS@users.noreply.github.com>
2021-03-01 18:42:52 -0600

    Update alertMaintenance.js

--- commands/developer/alertMaintenance.js
+++ commands/developer/alertMaintenance.js
@@ -3,8 +3,14 @@ module.exports = {
     category: 'developer',
     description: 'Warns staff about bot maintenance',
     args: true,
-    guildOnly: true,
+    guildOnly: false,
     async execute(client, message, args) {
-        if(msg.content
+        const alertMaintenance = new Discord.MessageEmbed() 
+            .setTitle('Maintenance Alert')
+            .setDescription(msg.author.username + ' warns of a scheduled bot maintenance!)
+            .setColor('#c186ff')
+            .setFooter('Cosmic Events')
+            .setTimestamp(msg.createdTimestamp);
+        msg.channel.send(alertMaintenance);
 }
 }
