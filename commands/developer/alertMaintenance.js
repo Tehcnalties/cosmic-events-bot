6b62bd0f068297651244f1003e2430662c614d64
AI2CTURUS <78950532+AI2CTURUS@users.noreply.github.com>
2021-03-01 18:37:16 -0600

    Update and rename alertMaintenance to alertMaintenance.js

---
+++ commands/developer/alertMaintenance.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'alertMaintenance',
+    category: 'developer',
+    description: 'Warns staff about bot maintenance',
+    args: true,
+    guildOnly: true,
+    async execute(client, message, args) {
+        if(msg.content
+}
+}
